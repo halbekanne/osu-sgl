41b0d7f37ae76494b3c8a779fbfc1cab52d7b171
Dominik Halfkann <Dominik.Halfkann@googlemail.com>
2011-05-15 17:46:30 +0200

    Parser can evaluate math expressions and can generate output. Example in SGLParserTestProject

---
+++ SGLParserTestProject/SGLParserTester/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.compileBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(35, 21);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(652, 223);
+            this.inputBox.TabIndex = 0;
+            this.inputBox.Text = "int x = 3*(2+4)/(8-2); /* Ergebniss = 3 */";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(35, 302);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(652, 223);
+            this.outputBox.TabIndex = 1;
+            // 
+            // compileBtn
+            // 
+            this.compileBtn.Location = new System.Drawing.Point(285, 250);
+            this.compileBtn.Name = "compileBtn";
+            this.compileBtn.Size = new System.Drawing.Size(147, 46);
+            this.compileBtn.TabIndex = 2;
+            this.compileBtn.Text = "Übersetze SGL";
+            this.compileBtn.UseVisualStyleBackColor = true;
+            this.compileBtn.Click += new System.EventHandler(this.compileBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 541);
+            this.Controls.Add(this.compileBtn);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "SGL Parser Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button compileBtn;
+    }
+}
+
---
+++ SGLParserTestProject/SGLParserTester/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using SGL;
+using Antlr.Runtime;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void compileBtn_Click(object sender, EventArgs e)
+        {
+
+            ANTLRStringStream sStream = new ANTLRStringStream(inputBox.Text);
+            SGLLexer lexer = new SGLLexer(sStream);
+
+            CommonTokenStream tStream = new CommonTokenStream(lexer);
+
+            SGLParser parser = new SGLParser(tStream);
+            try
+            {
+                parser.compilationUnit();
+                outputBox.Text = parser.GetOutput();
+            }
+            catch (Exception ex)
+            {
+                outputBox.Text = "Es ist ein Fehler aufgetreten.";
+                Console.Error.WriteLine(ex.StackTrace);
+            }
+
+
+            
+        }
+    }
+}
