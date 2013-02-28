0b07983ba178aceda4dc5a74f78257764fef95d0
Dominik Halfkann <Dominik.Halfkann@googlemail.com>
2013-02-28 01:14:46 +0100

    blargh

---
+++ SGLEditor/Frontend/IItem.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SGLTest
+{
+    public interface IItem
+    {
+        Guid Id
+        {
+            get;
+        }
+
+        string Filename
+        {
+            get;
+            set;
+        }
+
+        EItemType ItemType
+        {
+            get;
+            set;
+        }
+
+        void RefreshType();
+    }
+
+    public enum EItemType
+    {
+        Default,
+        Picture
+    }
+
+    public class Item : IItem
+    {
+        public Item()
+        {
+            id = Guid.NewGuid();
+        }
+
+        Guid id;
+
+        public Guid Id
+        {
+            get { return id; }
+        }
+
+        string filename;
+
+        public string Filename
+        {
+            get { return filename; }
+            set
+            {
+                filename = value;
+                RefreshType();
+            }
+        }
+
+        EItemType itemType;
+
+        public EItemType ItemType
+        {
+            get { return itemType; }
+            set { itemType = value; }
+        }
+
+        public void RefreshType()
+        {
+            FileInfo info = new FileInfo(Filename);
+            ItemType = ItemTypeFactory.GetItemType(info.Extension);
+        }
+
+
+    }
+
+    class ItemTypeFactory
+    {
+        static string[] pictures = { ".png", ".bmp", ".jpg", ".gif" };
+        public static EItemType GetItemType(string aFileExtension)
+        {
+            EItemType result = EItemType.Default;
+            List<string> pictureList = new List<string>();
+            pictureList.AddRange(pictures);
+
+            if (pictureList.Contains(aFileExtension))
+            {
+                result = EItemType.Picture;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SGLEditor/Frontend/IPreviewUserControl.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SGLTest
+{
+    public interface IPreviewUserControl
+    {
+        void Add(IItem anItem);
+        void Remove(IItem anItem);
+        void ClearItems();
+    }
+
+    public class PreviewUserControl : FlowLayoutPanel, IPreviewUserControl
+    {
+        public PreviewUserControl()
+        {
+            items = new List<IItem>();
+        }
+
+        IList<IItem> items;
+
+        // add an item to items 
+        public void Add(IItem anItem)
+        {
+            items.Add(anItem);
+            AddControl(anItem);
+        }
+
+        // remove an item from items 
+        public void Remove(IItem anItem)
+        {
+            items.Remove(anItem);
+            RemoveControl(anItem);
+        }
+
+        //add preview picture box
+        private void AddControl(IItem anItem)
+        {
+            this.Controls.Add(new PreviewPictureBox(anItem));
+        }
+
+        //remove preview picture box
+        private void RemoveControl(IItem anItem)
+        {
+            for (int i = this.Controls.Count - 1; i >= 0; i--)
+            {
+                if (this.Controls[i] is PreviewPictureBox)
+                {
+                    if ((this.Controls[i] as PreviewPictureBox).PreviewItem.Id == anItem.Id)
+                    {
+                        this.Controls.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Clear list
+        public void ClearItems()
+        {
+            items.Clear();
+            for (int i = this.Controls.Count - 1; i >= 0; i--)
+            {
+                if (this.Controls[i] is PreviewPictureBox)
+                {
+                    this.Controls.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    class PreviewPictureBox : PictureBox
+    {
+        public PreviewPictureBox(IItem anItem)
+        {
+            previewItem = anItem;
+            base.SizeMode = PictureBoxSizeMode.Zoom;
+            this.Width = 100; //default width
+            this.Height = 60; // default width;
+            this.Click += new EventHandler(PreviewPictureBox_Click);
+        }
+
+        void PreviewPictureBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(PreviewItem.Filename);
+        }
+
+        IItem previewItem;
+
+        public IItem PreviewItem
+        {
+            get { return previewItem; }
+        }
+
+        Image image;
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            if (image == null)
+            {
+                switch (PreviewItem.ItemType)
+                {
+                    case EItemType.Default:
+                        image = Image.FromFile(@"c:\default.png");
+                        break;
+                    case EItemType.Picture:
+                        image = Image.FromFile(PreviewItem.Filename);
+                        break;
+                }
+                this.Image = image;
+            }
+
+        }
+    }
+}
